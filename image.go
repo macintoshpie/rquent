162e54d63143154ae8c885e1682e946d73537bdd
Ted Summer <ted.summer2@gmail.com>
2019-09-11 11:06:29 -0600

    refactor(image): factor out updating most frequent color slice

--- image.go
+++ image.go
@@ -47,6 +47,33 @@ func newClient(timeout time.Duration) *http.Client {
 // Used to indicate a color that's not from the source image; should not be modified
 var PlaceholderColor = color.NRGBA{}
 
+// update the most frequent colors slice - assumed the slice is in sorted descending order by counts
+func updateMostFrequentColors(mostColors []color.NRGBA, c color.NRGBA, counts map[color.NRGBA]uint64) {
+	if c == mostColors[0] || c == mostColors[1] || c == mostColors[2] {
+		// case 1: color is already one of the most frequent - check if it needs to be swapped
+		for j := 1; j < 3; j += 1 {
+			if c == mostColors[j] && counts[c] > counts[mostColors[j-1]] {
+				mostColors[j-1], mostColors[j] = mostColors[j], mostColors[j-1]
+				break
+			}
+		}
+	} else {
+		// case 2: color is not one of the most frequent
+		//   if color counts is less than or equal to all of the most frequent, do nothing
+		//   otherwise, it's now one of the most frequent and we update the slice accordingly
+		if counts[c] > counts[mostColors[2]] {
+			for i := 0; i < 3; i += 1 {
+				if mostColors[i] == PlaceholderColor {
+					mostColors[i] = c
+					break
+				} else if i == 2 {
+					mostColors[2] = c
+				}
+			}
+		}
+	}
+}
+
 // Return slice of colors in sorted order of prevalence
 func getPrevalentColors(imgPtr *image.Image) (colorSummary, error) {
 	// TODO: generalize to k most prevalent, use a min-heap
@@ -64,29 +91,8 @@ func getPrevalentColors(imgPtr *image.Image) (colorSummary, error) {
 			c.A = 255
 			counts[c] += 1
 
-			// TODO: consider factoring out into function
-			// update most frequent colors as needed
-			if c == mostColors[0] || c == mostColors[1] || c == mostColors[2] {
-				// case 1: color is already one of the most frequent - check if it needs to be swapped
-				for j := 1; j < 3; j += 1 {
-					if c == mostColors[j] && counts[c] > counts[mostColors[j-1]] {
-						mostColors[j-1], mostColors[j] = mostColors[j], mostColors[j-1]
-						break
-					}
-				}
-			} else {
-				// case 2: color is not one of the most frequent - insert at first empty slot or the end
-				if counts[c] > counts[mostColors[2]] {
-					for i := 0; i < 3; i += 1 {
-						if mostColors[i] == PlaceholderColor {
-							mostColors[i] = c
-							break
-						} else if i == 2 {
-							mostColors[2] = c
-						}
-					}
-				}
-			}
+			// update most frequent colors
+			updateMostFrequentColors(mostColors, c, counts)
 		}
 	}
 
